3c88364b66da3c00a73f6db967a6f1f918a40e76
John Lago <750845+Lagoja@users.noreply.github.com>
2019-09-27 19:22:18 -0700

    Initial Commit

---
+++ Interpreter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Interpreter
+//
+//  Created by John Lago on 9/27/19.
+//  Copyright © 2019 John Lago. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
